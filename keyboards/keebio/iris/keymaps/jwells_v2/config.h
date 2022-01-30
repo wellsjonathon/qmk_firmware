2e4ac6069ccd96420ced960529334f9673ee8dda
wellsjonathon <wellsjonathonr@gmail.com>
2022-01-30 14:16:22 -0600

    Iris v2 revisions

--- keyboards/keebio/iris/keymaps/jwells_v2/config.h
+++ keyboards/keebio/iris/keymaps/jwells_v2/config.h
@@ -19,3 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+#define IGNORE_MOD_TAP_INTERRUPT
